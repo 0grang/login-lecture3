8d685fbc1c355c2cbc0774f33c5173d1a79c7431
grang <shee0201@naver.com>
2022-03-13 19:15:37 +0900

    프런트단 javascript 만들기

--- app/app.js
+++ app/app.js
@@ -25,6 +25,8 @@ const home = require("./src/routes/home");
 //앱 셋팅
 app.set("views","./src/views");
 app.set("view engine","ejs") //views엔진을 ejs를 이용하겠다는 뜻
+app.use(express.static(`${__dirname}/src/public`));
+
 
 //아래 라우팅하는 것을 모두 가져가서  routes / home/ indexedDB.js로 이동
 // app.get("/",(req,res)=>{
