2de2c430a4902f18dfbd8331598eeec2d5ce63b6
grang <shee0201@naver.com>
2022-03-14 22:07:55 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,17 @@
-"use strict"
-console.log("hello");
+"use strict";
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"), //태그에 id의 값이 psword로 되어있는 것을 불러올때 #~(이것 전체가 선택자)
+    loginBtn = document.querySelector("button"); //태그 그대로 이름 불러오기 
+
+    
+loginBtn.addEventListener("click",login);
+
+function login(){
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+  console.log(req);
+}
+
